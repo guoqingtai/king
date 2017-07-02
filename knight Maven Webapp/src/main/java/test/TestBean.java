350f6e583991fe5fae397b0bf9cd7887fc20e4da
guoqingtai <guoqingtai@yeah.net>
2017-07-02 17:54:55 +0800

    增加简单的spring
    增加spring

---
+++ knight Maven Webapp/src/main/java/test/TestBean.java
@@ -0,0 +1,13 @@
+package test;
+
+public class TestBean {
+	private String test ="123";
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+}
