830f9947db0d706531e033bcf6482f2ddd8f4b08
guoqingtai <guoqingtai@yeah.net>
2017-09-29 23:15:11 +0800

    提交

--- knight Maven Webapp/src/main/java/knight/a0_controllers/LoginController.java
+++ knight Maven Webapp/src/main/java/knight/a0_controllers/LoginController.java
@@ -1,53 +1,72 @@
 package knight.a0_controllers;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import knight.b0_mnt.entity.TResource;
 import knight.b0_mnt.service.LoginService;
 
 @Controller()
 @RequestMapping("/")
 public class LoginController {
 
-	@Autowired BookDaoTest tst;
-	@Autowired LoginService svs;
-	
+	@Autowired
+	BookDaoTest tst;
+	@Autowired
+	LoginService svs;
+
 	@RequestMapping("/login.htm")
 	public String login(HttpServletRequest request) {
 		try {
-			tst.testQueryById() ;
+			tst.testQueryById();
 			svs.getUser("admin");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return "login";
-		
 	}
-	
+
 	@RequestMapping("/index.htm")
 	public String index(HttpServletRequest request) {
 		return "index";
 	}
-	
-	
+
+	@RequestMapping("/menu.htm")
+	public String menu(HttpServletRequest request) {
+		String menuName = request.getParameter("uri");
+		System.out.println("MENU:" + menuName);
+		return menuName;
+	}
+
+	//@RequestMapping("/ajaxlist")
+	//@ResponseBody // (springmvc的Jackson注解，返回json字符串)
+	//public List<TResource> getResourceList() {
+	//	List<User> list = svs.getResourceByRoleId(roleId);
+	//	return list;
+	//}
+
 	@RequestMapping("/test.htm")
 	public String test(HttpServletRequest request) {
 		String userName = request.getParameter("userName");
 		System.out.println(userName);
-		//BookDaoTest bkd = new BookDaoTest();
+		// BookDaoTest bkd = new BookDaoTest();
 		try {
-			tst.testQueryById() ;
+			tst.testQueryById();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "index";
-		
+
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- knight Maven Webapp/src/main/java/knight/b0_mnt/entity/TResource.java
+++ knight Maven Webapp/src/main/java/knight/b0_mnt/entity/TResource.java
@@ -5,6 +5,37 @@ public class TResource {
 	private String resId;
 	private String uri;
 	
+	private int num;
+	
+	private int parent;
+	
+	private String zh;
+	
+	private String en;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	public String getZh() {
+		return zh;
+	}
+	public void setZh(String zh) {
+		this.zh = zh;
+	}
+	public String getEn() {
+		return en;
+	}
+	public void setEn(String en) {
+		this.en = en;
+	}
 	public String getResId() {
 		return resId;
 	}
